224aaeb730568520abc4ee2f824f98a01e931949
vikas goudar <vikasgoudar123@gmail.com>
2026-02-26 23:06:36 +0530

    update

---
+++ go/basics/temp_files/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var global = "vikas"
+
+func main() {
+	// var age int
+	// var name string = "vikas"
+	// var name2 = "goudar"
+	//
+	// count := 0
+
+	printName()
+}
+
+func printName() {
+	fmt.Println(global)
+}
